91f13301eeb0de694be15d345ec085b5e9764769
David Fant <david@fant.io>
2021-11-26 10:53:23 +0000

    set up ant design

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withLess = require("next-with-less");
+
+module.exports = withLess({
   reactStrictMode: true,
-}
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+// import 'antd/dist/antd.css';
+// import 'antd/lib/style/themes/default.css';
+import '../styles/globals.less'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
