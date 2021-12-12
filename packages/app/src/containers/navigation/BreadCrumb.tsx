357875d670e7bd3376873faa1c8855f1f4e3e1a1
Gustaf Broman <gustaf.broman@tink.com>
2021-12-12 22:12:47 +0100

    feat: add cta to header

--- packages/app/src/containers/navigation/BreadCrumb.tsx
+++ packages/app/src/containers/navigation/BreadCrumb.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from "react";
 import Link from "next/link";
 import Breadcrumb, { Route } from "antd/lib/breadcrumb/Breadcrumb";
-import { Skeleton } from "antd";
+import { Button, Row, Skeleton } from "antd";
+import { useAuthContext } from "@dewo/app/contexts/AuthContext";
 
 const itemRender = (route: Route, _params: any, routes: any, paths: any) => {
   const last = routes.indexOf(route) === routes.length - 1;
@@ -16,9 +17,15 @@ type BreadCrumbProps = {
   routes?: Route[];
 };
 
-export const BreadCrumb: FC<BreadCrumbProps> = ({ routes }) =>
-  !!routes ? (
-    <Breadcrumb itemRender={itemRender} routes={routes} />
+export const BreadCrumb: FC<BreadCrumbProps> = ({ routes }) => {
+  const { user } = useAuthContext();
+
+  return !!routes ? (
+    <Row style={{ display: "flex", justifyContent: "space-between" }}>
+      <Breadcrumb itemRender={itemRender} routes={routes} />
+      {!user && <Button type="primary">Sign in</Button>}
+    </Row>
   ) : (
     <Skeleton loading active title={false} paragraph={{ rows: 1 }} />
   );
+};
