b89b0b9f5855e5639ff29a9ea00693be7844ea68
Gabriel Petersson <gabriielpetersson@gmail.com>
2021-12-16 00:30:59 +0200

    fix nit

--- packages/api/src/modules/user/dto/CreateUserDetail.ts
+++ packages/api/src/modules/user/dto/CreateUserDetail.ts
@@ -1,12 +1,11 @@
 import { UserDetailType } from "@dewo/api/models/UserDetail";
 import { Field, InputType } from "@nestjs/graphql";
-import { GraphQLString } from "graphql";
 
 @InputType()
 export class CreateUserDetailInput {
   @Field(() => UserDetailType)
   public type!: UserDetailType;
 
-  @Field(() => GraphQLString)
+  @Field()
   public value!: string;
 }
