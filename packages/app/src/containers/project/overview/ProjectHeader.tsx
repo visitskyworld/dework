2546a28ddec9826e3bd17627ce4276c84c6cbbb4
sanjay radadiya <sanjay.radadiya91@gmail.com>
2021-12-17 20:25:34 +0530

    Change way of project editing

--- packages/app/src/containers/project/overview/ProjectHeader.tsx
+++ packages/app/src/containers/project/overview/ProjectHeader.tsx
@@ -1,5 +1,5 @@
-import { Avatar, PageHeader, Row, Skeleton, Typography } from "antd";
-import React, { FC, useMemo } from "react";
+import { Avatar, Input, PageHeader, Row, Skeleton, Typography } from "antd";
+import React, { FC, useCallback, useEffect, useMemo, useState } from "react";
 import _ from "lodash";
 import { useProject, useUpdateProject } from "../hooks";
 import { UserAvatar } from "@dewo/app/components/UserAvatar";
@@ -8,6 +8,7 @@ import { InviteButton } from "../../invite/InviteButton";
 import { Route } from "antd/lib/breadcrumb/Breadcrumb";
 import { PageHeaderBreadcrumbs } from "../../navigation/PageHeaderBreadcrumbs";
 import { JoinOrganizationButton } from "../../organization/overview/JoinOrganizationButton";
+import { usePermission } from "@dewo/app/contexts/PermissionsContext";
 
 interface Props {
   projectId: string;
@@ -17,6 +18,9 @@ export const ProjectHeader: FC<Props> = ({ projectId }) => {
   const project = useProject(projectId);
   const organization = useOrganization(project?.organizationId);
   const updateProject = useUpdateProject();
+  const canEdit = usePermission("update", "Project");
+  const [isEdit, setIsEdit] = useState(false);
+  const [projectName, setProjectName] = useState("");
 
   const routes = useMemo(
     () =>
@@ -39,24 +43,45 @@ export const ProjectHeader: FC<Props> = ({ projectId }) => {
     [organization, project]
   ) as Route[];
 
-  const editProject = _.debounce((text: string | null) => {
-    updateProject({
-      id: projectId,
-      name: text,
-    });
+  const handleChange = useCallback(async (e: any) => {
+    setProjectName(e.target.value);
+    updateProjectName(e.target.value);
+  }, []);
+
+  const updateProjectName = _.debounce((name) => {
+    if (name) {
+      updateProject({
+        id: projectId,
+        name: name,
+      });
+    }
   }, 500);
+
+  useEffect(() => {
+    if (!!project) {
+      setProjectName(project.name);
+    }
+  }, [project]);
 
   return (
     <PageHeader
       title={
         !!project ? (
           <Typography.Title level={3} style={{ margin: 0 }}>
-            <div
-              contentEditable="true"
-              onInput={(e) => editProject(e.currentTarget.textContent)}
-            >
-              {project.name}
-            </div>
+            {!isEdit ? (
+              <div onClick={() => setIsEdit(true)}>{project.name}</div>
+            ) : (
+              <Input
+                disabled={!canEdit}
+                autoFocus={true}
+                className="dewo-field dewo-field-display ant-typography-h3"
+                placeholder={`Enter a project name...`}
+                onBlur={() => setIsEdit(false)}
+                onChange={handleChange}
+                value={projectName}
+                bordered={false}
+              />
+            )}
           </Typography.Title>
         ) : (
           <Skeleton.Button active style={{ width: 200 }} />
