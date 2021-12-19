8893d48d9cf93f080864705dfb48c87b67e299ff
David Fant <david@fant.io>
2021-12-19 15:05:29 +0000

    gnosis safe experimentation cleanup

--- packages/app/src/util/gnosis.ts
+++ packages/app/src/util/gnosis.ts
@@ -12,3 +12,31 @@ export function useIsGnosisSafeOwner(): (
     return safe.isOwner(await signer.getAddress());
   }, []);
 }
+
+/*
+
+      const tx = await safeSdk.createTransaction([
+        {
+          to: "0x761996F7258A19B6aCcF6f22e9Ca8CdAA92D75A6",
+          value: ethers.utils.parseEther("0.001").toString(),
+          // TODO(fant): figure out what this is... Should it refer to e.g. the Dework task id?
+          // data: `0x${"watafak should be here"}`,
+          data: "0x761996F7258A19B6aCcF6f22e9Ca8CdAA92D75A6",
+        },
+      ]);
+
+      console.warn("created tx");
+
+      // await safeSdk.signTransaction(tx);
+      // await safeSdk.signTransaction(tx);
+
+      const txHash = await safeSdk.getTransactionHash(tx);
+      console.warn({ tx, txHash });
+
+      // const response = await safeSdk.approveTransactionHash(txHash);
+      // console.warn({ response });
+
+      const executeTxResponse = await safeSdk.executeTransaction(tx);
+      console.warn({ executeTxResponse });
+
+*/
