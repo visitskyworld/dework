188763d9ed16d0f496c4a7a54da713342587c8e1
David Fant <david@fant.io>
2021-12-28 13:07:47 +0100

    landing page v2 done

--- packages/app/src/containers/landingPage/LandingPage.tsx
+++ packages/app/src/containers/landingPage/LandingPage.tsx
@@ -3,54 +3,31 @@ import * as Icons from "@ant-design/icons";
 import {
   Button,
   Image,
-  Card,
   Col,
   Divider,
   PageHeader,
   Row,
-  Skeleton,
   Space,
   Typography,
   Modal,
+  Tabs,
+  Avatar,
 } from "antd";
-import React, { FC, useCallback, useMemo, useRef } from "react";
+import React, { FC, useCallback, useRef } from "react";
 import { useFeaturedOrganizations } from "../organization/hooks";
 import { OrganizationCard } from "./OrganizationCard";
-import { siteTitle, siteDescription } from "@dewo/app/util/constants";
-import { TaskCard } from "../project/board/TaskCard";
-import { useTasks } from "../task/hooks";
-import { Task, TaskStatusEnum } from "@dewo/app/graphql/types";
-import { TaskUpdateModalListener } from "../task/TaskUpdateModal";
+import { siteTitle } from "@dewo/app/util/constants";
 import { DeworkIcon } from "@dewo/app/components/icons/Dework";
-import _ from "lodash";
 import { LoginButton } from "../auth/LoginButton";
 import { useToggle } from "@dewo/app/util/hooks";
 import YouTube from "react-youtube";
+import { DiscordIcon } from "@dewo/app/components/icons/Discord";
 
 const NUM_COLUMNS = 3;
 
 export const LandingPage: FC = () => {
   const { user } = useAuthContext();
   const organizations = useFeaturedOrganizations(4);
-  const latestTasks = useTasks(
-    useMemo(
-      () => ({
-        statuses: [TaskStatusEnum.TODO],
-        limit: 100,
-        organizationIds: organizations?.map((o) => o.id),
-      }),
-      [organizations]
-    ),
-    !organizations
-  );
-  const latestTaskChunks = useMemo(() => {
-    if (!latestTasks) return [];
-    const chunks = _.range(NUM_COLUMNS).map<Task[]>(() => []);
-    latestTasks.forEach((task, index) => {
-      chunks[index % NUM_COLUMNS].push(task);
-    });
-    return chunks;
-  }, [latestTasks]);
 
   const watchVideoDemo = useToggle();
   const youtubeRef = useRef<YouTube>(null);
@@ -58,151 +35,6 @@ export const LandingPage: FC = () => {
     watchVideoDemo.toggleOff();
     youtubeRef.current?.getInternalPlayer().pauseVideo();
   }, [watchVideoDemo]);
-
-  if (Math.random()) {
-    return (
-      <>
-        <PageHeader
-          title={
-            <Row align="middle">
-              <DeworkIcon style={{ width: 24, height: 24, marginRight: 8 }} />
-              <Typography.Title level={4} style={{ margin: 0 }}>
-                {siteTitle}
-              </Typography.Title>
-            </Row>
-          }
-          extra={[
-            !user && (
-              <LoginButton key="log-in" type="text">
-                Log In
-              </LoginButton>
-            ),
-            !user && (
-              <LoginButton key="get-started" type="primary">
-                Get Started
-              </LoginButton>
-            ),
-          ]}
-          style={{ width: "100%" }}
-          className="max-w-xl mx-auto"
-        />
-        <Row className="max-w-xl mx-auto" style={{ width: "100%" }}>
-          <Col md={12} xs={24} style={{ padding: "96px 24px" }}>
-            <Space direction="vertical" size="large">
-              <Typography.Title level={1}>
-                A web3-native Asana with crypto payments, credentialing,
-                bounties and more
-              </Typography.Title>
-              <Row>
-                <Row style={{ width: 100 }}>
-                  <Divider />
-                </Row>
-                <Typography.Paragraph style={{ fontSize: "150%" }}>
-                  Manage your tasks and bounties in one place. Get contributor
-                  applicants, sync with Discord, boost the reputation of
-                  contributors, and pay with your own DAO's tokens
-                </Typography.Paragraph>
-              </Row>
-
-              <Space>
-                <LoginButton type="primary" size="large">
-                  Get Started
-                </LoginButton>
-                <Button
-                  type="text"
-                  size="large"
-                  icon={<Icons.PlayCircleOutlined />}
-                  onClick={watchVideoDemo.toggleOn}
-                >
-                  Watch Video
-                </Button>
-              </Space>
-            </Space>
-          </Col>
-          <Col
-            md={12}
-            xs={24}
-            style={{ padding: 24, display: "grid", placeItems: "center" }}
-          >
-            <Image
-              width="100%"
-              src={
-                "https://moralis.io/wp-content/uploads/2021/07/new_homepage_img.svg"
-              }
-              preview={false}
-            />
-            {/* <Tabs centered type="line" className="dewo-lp-feature-tabs">
-              <Tabs.TabPane
-                tab="Feature X"
-                key="feature-x"
-                style={{ padding: 8 }}
-              >
-                <Image
-                  width="100%"
-                  src="https://storage.googleapis.com/assets.dework.xyz/uploads/7c830d17-de1d-4245-a032-c4723d9c6a25/image.png"
-                />
-                <Typography.Paragraph
-                  type="secondary"
-                  style={{
-                    textAlign: "center",
-                    fontSize: "130%",
-                    marginTop: 16,
-                  }}
-                >
-                  Feature X is a feature that will be released soon.
-                </Typography.Paragraph>
-              </Tabs.TabPane>
-              <Tabs.TabPane tab="Feature Y" key="feature-y">
-                Content of Tab Pane 1
-              </Tabs.TabPane>
-              <Tabs.TabPane tab="Feature Z" key="feature-z">
-                Content of Tab Pane 1
-              </Tabs.TabPane>
-            </Tabs> */}
-          </Col>
-        </Row>
-        <Row
-          style={{
-            padding: "64px 24px",
-            backgroundColor: "rgba(255,255,255,0.1)",
-          }}
-        >
-          <Col className="max-w-lg mx-auto" style={{ width: "100%" }}>
-            <Typography.Title
-              level={4}
-              style={{ textAlign: "center", width: "100%" }}
-            >
-              Explore some popular DAOs
-            </Typography.Title>
-            <Row gutter={[16, 16]} style={{ margin: 0, width: "100%" }}>
-              {organizations?.map((org) => (
-                <Col xs={24} md={24 / NUM_COLUMNS} key={org.id}>
-                  <OrganizationCard organization={org} />
-                </Col>
-              ))}
-            </Row>
-          </Col>
-        </Row>
-        <Modal
-          visible={watchVideoDemo.isOn}
-          footer={null}
-          bodyStyle={{ padding: 0 }}
-          width="min(1280px, 100vw)"
-          onCancel={closeVideoDemoModal}
-        >
-          <YouTube
-            ref={youtubeRef}
-            videoId="66SaEDzlmP4"
-            opts={{
-              width: "100%",
-              height: "720",
-              playerVars: { autoplay: 1 },
-            }}
-          />
-        </Modal>
-      </>
-    );
-  }
 
   return (
     <>
@@ -215,102 +47,166 @@ export const LandingPage: FC = () => {
             </Typography.Title>
           </Row>
         }
-        extra={!user && <LoginButton type="primary">Sign in</LoginButton>}
-      />
-      <Divider style={{ margin: 0 }} />
-
-      <Row style={{ backgroundColor: "rgba(255,255,255,0.1)" }}>
-        <Row className="max-w-md mx-auto" style={{ padding: 32 }}>
-          <Typography.Title
-            level={1}
-            style={{ width: "100%", maxWidth: "100%", textAlign: "center" }}
-          >
-            {siteTitle}
-          </Typography.Title>
-          <Typography.Paragraph style={{ textAlign: "center", width: "100%" }}>
-            {siteDescription}
-          </Typography.Paragraph>
-          {!user && (
-            <LoginButton type="primary" size="large" className="mx-auto">
-              Log in
+        extra={[
+          !user && (
+            <LoginButton key="log-in" type="text">
+              Log In
             </LoginButton>
-          )}
-        </Row>
-      </Row>
-      <Divider style={{ margin: 0 }} />
+          ),
+          !user && (
+            <LoginButton key="get-started" type="primary">
+              Get Started
+            </LoginButton>
+          ),
+        ]}
+        style={{ width: "100%" }}
+        className="max-w-xl mx-auto"
+      />
+      <Row className="max-w-xl mx-auto" style={{ width: "100%" }}>
+        <Col md={12} xs={24} style={{ padding: "96px 24px" }}>
+          <Space direction="vertical" size="large">
+            <Typography.Title level={1}>
+              A web3-native Asana with crypto payments, credentialing, bounties
+              and more
+            </Typography.Title>
+            <Row>
+              <Row style={{ width: 100 }}>
+                <Divider />
+              </Row>
+              <Typography.Paragraph style={{ fontSize: "150%" }}>
+                Manage your tasks and bounties in one place. Get contributor
+                applicants, sync with Discord, boost the reputation of
+                contributors, and pay with your own DAO's tokens
+              </Typography.Paragraph>
+            </Row>
 
-      <Row className="max-w-md mx-auto">
-        <Typography.Title
-          level={4}
-          style={{ textAlign: "center", width: "100%", marginTop: 32 }}
-        >
-          🏆 Popular DAOs
-        </Typography.Title>
-        <Row gutter={[16, 16]} style={{ margin: 0, width: "100%" }}>
-          {organizations?.map((org) => (
-            <Col xs={24} md={24 / NUM_COLUMNS} key={org.id}>
-              <OrganizationCard organization={org} />
-            </Col>
-          ))}
-        </Row>
-
-        <Typography.Title
-          level={4}
-          style={{ textAlign: "center", width: "100%", marginTop: 48 }}
-        >
-          🔥 Newest tasks
-        </Typography.Title>
-        <Row gutter={[16, 16]} style={{ margin: 0, width: "100%" }}>
-          {latestTaskChunks.map((chunk, index) => (
-            <Col xs={24} md={24 / NUM_COLUMNS} key={index}>
-              <Space
-                direction="vertical"
-                size="middle"
-                style={{ width: "100%", paddingBottom: 8 }}
+            <Space>
+              <LoginButton type="primary" size="large">
+                Get Started
+              </LoginButton>
+              <Button
+                type="text"
+                size="large"
+                icon={<Icons.PlayCircleOutlined />}
+                onClick={watchVideoDemo.toggleOn}
               >
-                {!latestTasks &&
-                  _.range(3).map(() => (
-                    <Card size="small">
-                      <Skeleton loading />
-                    </Card>
-                  ))}
-                {chunk.map((task) => (
-                  <TaskCard
-                    key={task.id}
-                    task={task}
-                    style={{ cursor: "pointer" }}
-                  />
-                ))}
-              </Space>
-            </Col>
-          ))}
-        </Row>
-        <TaskUpdateModalListener />
+                Watch Video
+              </Button>
+            </Space>
+          </Space>
+        </Col>
+        <Col
+          md={12}
+          xs={24}
+          style={{ padding: 24, display: "grid", placeItems: "center" }}
+        >
+          <Tabs centered type="line" className="dewo-lp-feature-tabs">
+            <Tabs.TabPane
+              tab="Crypto Payments"
+              key="crypto-payments"
+              style={{ padding: 8 }}
+            >
+              <Image width="100%" src="/lp/crypto-payments.jpeg" />
+              <Typography.Paragraph
+                type="secondary"
+                style={{
+                  textAlign: "center",
+                  fontSize: "130%",
+                  marginTop: 16,
+                }}
+              >
+                Pay contributors directly with Metamask, Gnosis Safe or Phantom
+                Wallet
+              </Typography.Paragraph>
+            </Tabs.TabPane>
+            <Tabs.TabPane
+              tab="Review Contributors"
+              key="review-contributors"
+              style={{ padding: 8 }}
+            >
+              <Image width="100%" src="/lp/review-contributors.jpeg" />
+              <Typography.Paragraph
+                type="secondary"
+                style={{
+                  textAlign: "center",
+                  fontSize: "130%",
+                  marginTop: 16,
+                }}
+              >
+                Let contributors find and apply to tasks. See their work history
+                and assign the task to the most capable.
+              </Typography.Paragraph>
+            </Tabs.TabPane>
+          </Tabs>
+        </Col>
       </Row>
+      <Row
+        style={{
+          padding: "64px 24px",
+          backgroundColor: "rgba(255, 255, 255, 0.1)",
+        }}
+      >
+        <Col className="max-w-lg mx-auto" style={{ width: "100%" }}>
+          <Typography.Title
+            level={4}
+            style={{ textAlign: "center", width: "100%" }}
+          >
+            Explore some popular DAOs
+          </Typography.Title>
+          <Row gutter={[16, 16]} style={{ margin: 0, width: "100%" }}>
+            {organizations?.map((org) => (
+              <Col xs={24} md={24 / NUM_COLUMNS} key={org.id}>
+                <OrganizationCard organization={org} />
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+      <Row
+        className="max-w-xl mx-auto"
+        style={{ padding: "64px 24px", width: "100%" }}
+      >
+        <Space>
+          <Typography.Text>
+            Have questions or feedback? Reach out on Twitter or Discord
+          </Typography.Text>
+          <a
+            href="https://twitter.com/deworkxyz"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Avatar size="small">
+              <Icons.TwitterOutlined />
+            </Avatar>
+          </a>
+          <a
+            href="https://discord.gg/gvfMCyxs"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Avatar size="small">
+              <DiscordIcon />
+            </Avatar>
+          </a>
+        </Space>
+      </Row>
+      <Modal
+        visible={watchVideoDemo.isOn}
+        footer={null}
+        bodyStyle={{ padding: 0 }}
+        width="min(1280px, 100vw)"
+        onCancel={closeVideoDemoModal}
+      >
+        <YouTube
+          ref={youtubeRef}
+          videoId="4hwQznYw9FM"
+          opts={{
+            width: "100%",
+            height: "720",
+            playerVars: { autoplay: 1 },
+          }}
+        />
+      </Modal>
     </>
   );
-
-  // return (
-  //   <>
-  //     <MobileHeader asHeader={true} />
-  //     <Space
-  //       direction="vertical"
-  //       className="mx-auto dewo-landing-page-main"
-  //       style={{
-  //         marginTop: 40,
-  //         maxWidth: 300 * 4 + (3 + 2) * 16,
-  //         maxHeight: "100%",
-  //       }}
-  //     >
-  //       <Typography.Title
-  //         level={3}
-  //         style={{ textAlign: "center", width: "100%" }}
-  //       >
-  //         Your tasks for today
-  //       </Typography.Title>
-
-  //       <UserTaskBoard userId={user.id} />
-  //     </Space>
-  //   </>
-  // );
 };
