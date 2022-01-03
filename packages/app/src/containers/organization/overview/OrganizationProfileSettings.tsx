8797cdf083e06bbcc1f16adc50794c0790ba534a
Gustaf Broman <gustaf.broman@tink.com>
2022-01-03 22:23:33 +0100

    refactor: use form with try catch for org edit

--- packages/app/src/containers/organization/overview/OrganizationProfileSettings.tsx
+++ packages/app/src/containers/organization/overview/OrganizationProfileSettings.tsx
@@ -1,7 +1,8 @@
-import React, { FC, useCallback } from "react";
-import { Card, Space, Typography } from "antd";
+import React, { FC, useCallback, useState } from "react";
+import { Button, Card, Form, Input, message, Space } from "antd";
 import { useOrganization, useUpdateOrganization } from "../hooks";
 import { usePermission } from "@dewo/app/contexts/PermissionsContext";
+import { UpdateOrganizationInput } from "../../../graphql/types";
 
 interface Props {
   organizationId: string;
@@ -12,14 +13,31 @@ export const OrganizationProfileSettings: FC<Props> = ({ organizationId }) => {
   const canUpdateOrganization = usePermission("update", "Organization");
 
   const updateOrganization = useUpdateOrganization();
-  const updateName = useCallback(
-    (name: string) => updateOrganization({ id: organizationId, name }),
-    [updateOrganization, organizationId]
-  );
-  const updateDescription = useCallback(
-    (description: string) =>
-      updateOrganization({ id: organizationId, description }),
-    [updateOrganization, organizationId]
+
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = useCallback(
+    async (values: UpdateOrganizationInput) => {
+      try {
+        setLoading(true);
+        await updateOrganization({
+          id: organizationId,
+          name: values.name,
+          description: values.description,
+        });
+        message.success({
+          content: "Organization profile updated!",
+          type: "success",
+        });
+      } catch {
+        message.error({
+          content: "Failed to update organization profile",
+          type: "error",
+        });
+      } finally {
+        setLoading(false);
+      }
+    },
+    [organizationId, updateOrganization]
   );
 
   if (!organization) return null;
@@ -28,28 +46,41 @@ export const OrganizationProfileSettings: FC<Props> = ({ organizationId }) => {
     <Space
       direction="vertical"
       style={{ width: "100%", paddingLeft: 16, paddingRight: 16 }}
+      contentEditable={canUpdateOrganization}
     >
       <Card>
-        <Typography.Title
-          level={2}
-          editable={
-            canUpdateOrganization
-              ? { onChange: updateName, autoSize: true }
-              : undefined
-          }
+        <Form<UpdateOrganizationInput>
+          layout="vertical"
+          requiredMark={false}
+          onFinish={handleSubmit}
+          style={{ maxWidth: 480 }}
         >
-          {organization.name}
-        </Typography.Title>
-        <Typography.Paragraph
-          type="secondary"
-          editable={
-            canUpdateOrganization
-              ? { onChange: updateDescription, autoSize: true }
-              : undefined
-          }
-        >
-          {organization.description || "No description..."}
-        </Typography.Paragraph>
+          <Form.Item
+            label="Organization display name"
+            name="name"
+            initialValue={organization.name}
+            rules={[{ required: true, message: "Please enter a name" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Description"
+            name="description"
+            initialValue={organization.description}
+            rules={[{ required: false, message: "No description..." }]}
+          >
+            <Input.TextArea />
+          </Form.Item>
+          <Button
+            type="ghost"
+            htmlType="submit"
+            size="middle"
+            loading={loading}
+            disabled={!canUpdateOrganization}
+          >
+            Update profile
+          </Button>
+        </Form>
       </Card>
     </Space>
   );
