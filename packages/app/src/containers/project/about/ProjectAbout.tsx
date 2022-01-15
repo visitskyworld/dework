b7f303b69392038f76a5193b60cd87efc06b9fd4
rafaewaqarqazi <rafaewaqar@gmail.com>
2022-01-15 17:21:56 +0500

    Add Placeholder text in project about tab

--- packages/app/src/containers/project/about/ProjectAbout.tsx
+++ packages/app/src/containers/project/about/ProjectAbout.tsx
@@ -1,6 +1,7 @@
 import { MarkdownEditor } from "@dewo/app/components/markdownEditor/MarkdownEditor";
 import { usePermission } from "@dewo/app/contexts/PermissionsContext";
 import { ProjectDetails } from "@dewo/app/graphql/types";
+import { Space, Typography } from "antd";
 import React, { FC, useCallback } from "react";
 import { useUpdateProject } from "../hooks";
 
@@ -16,12 +17,36 @@ export const ProjectAbout: FC<Props> = ({ project }) => {
     [updateProject, project.id]
   );
 
-  return (
+  return project.description ? (
     <MarkdownEditor
       initialValue={project.description ?? undefined}
       editable={canUpdateProject}
       mode="update"
       onSave={handleSave}
     />
+  ) : (
+    <div>
+      <Typography.Title
+        level={4}
+        style={{ borderBottom: "1px solid white", paddingBottom: 10 }}
+      >
+        Insert your project description here
+      </Typography.Title>
+      <ul>
+        <Space direction="vertical" size={16}>
+          <li>
+            <Typography.Text type="secondary">
+              Embed any links, files or images that are descriptive of the
+              project
+            </Typography.Text>
+          </li>
+          <li>
+            <Typography.Text type="secondary">
+              Maybe even a FAQ about the project
+            </Typography.Text>
+          </li>
+        </Space>
+      </ul>
+    </div>
   );
 };
