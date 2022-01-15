c273c9564bcd60011acc667f7a19624fe39aca8b
rafaewaqarqazi <rafaewaqar@gmail.com>
2022-01-15 17:34:17 +0500

    Change Placeholder in project about

--- packages/app/src/containers/project/about/ProjectAbout.tsx
+++ packages/app/src/containers/project/about/ProjectAbout.tsx
@@ -1,7 +1,6 @@
 import { MarkdownEditor } from "@dewo/app/components/markdownEditor/MarkdownEditor";
 import { usePermission } from "@dewo/app/contexts/PermissionsContext";
 import { ProjectDetails } from "@dewo/app/graphql/types";
-import { Space, Typography } from "antd";
 import React, { FC, useCallback } from "react";
 import { useUpdateProject } from "../hooks";
 
@@ -16,37 +15,16 @@ export const ProjectAbout: FC<Props> = ({ project }) => {
     (description: string) => updateProject({ id: project.id, description }),
     [updateProject, project.id]
   );
+  const initialValue = `## Insert your project description here
+- Embed any links, files or images that are descriptive of the project
+- Maybe even a FAQ about the project`;
 
-  return project.description ? (
+  return (
     <MarkdownEditor
-      initialValue={project.description ?? undefined}
+      initialValue={project.description || initialValue}
       editable={canUpdateProject}
       mode="update"
       onSave={handleSave}
     />
-  ) : (
-    <div>
-      <Typography.Title
-        level={4}
-        style={{ borderBottom: "1px solid white", paddingBottom: 10 }}
-      >
-        Insert your project description here
-      </Typography.Title>
-      <ul>
-        <Space direction="vertical" size={16}>
-          <li>
-            <Typography.Text type="secondary">
-              Embed any links, files or images that are descriptive of the
-              project
-            </Typography.Text>
-          </li>
-          <li>
-            <Typography.Text type="secondary">
-              Maybe even a FAQ about the project
-            </Typography.Text>
-          </li>
-        </Space>
-      </ul>
-    </div>
   );
 };
