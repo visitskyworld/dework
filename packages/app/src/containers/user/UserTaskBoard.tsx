3b3a3440200b8cb89771dec7d6930966c93f4c37
David Fant <david@fant.io>
2022-01-24 18:08:58 +0100

    remove dao list at the bottom of UserTaskBoard TODO column

--- packages/app/src/containers/user/UserTaskBoard.tsx
+++ packages/app/src/containers/user/UserTaskBoard.tsx
@@ -5,11 +5,6 @@ import { TaskBoard } from "../task/board/TaskBoard";
 import { useUserTasks } from "./hooks";
 import { TaskBoardColumnEmptyProps } from "../task/board/TaskBoardColumnEmtpy";
 import { SkeletonTaskBoard } from "../task/board/SkeletonTaskBoard";
-import { Avatar, Card, Col, List, Row, Space } from "antd";
-import { TaskSectionTitle } from "../task/board/TaskSectionTitle";
-import { OrganizationCard } from "../landingPage/OrganizationCard";
-import { CreateOrganizationButton } from "../navigation/CreateOrganizationButton";
-import { useFeaturedOrganizations } from "../organization/hooks";
 
 interface Props {
   userId: string;
@@ -36,56 +31,8 @@ const empty: Partial<Record<TaskStatus, TaskBoardColumnEmptyProps>> = {
 
 export const UserTaskBoard: FC<Props> = ({ userId }) => {
   const tasks = useUserTasks(userId, "cache-and-network");
-  const organizations = useFeaturedOrganizations(3);
   return tasks ? (
-    <TaskBoard
-      tasks={tasks}
-      empty={empty}
-      footer={{
-        [TaskStatus.TODO]: (
-          <Row gutter={[8, 8]}>
-            {!!organizations?.length && (
-              <Col span={24}>
-                <TaskSectionTitle title="Explore DAOs" />
-                <Space
-                  direction="vertical"
-                  style={{ width: "100%", marginBottom: 8 }}
-                >
-                  {organizations.map((organization) => (
-                    <OrganizationCard
-                      key={organization.id}
-                      organization={organization}
-                      title={{ level: 5 }}
-                    />
-                  ))}
-                  <CreateOrganizationButton
-                    block
-                    type="text"
-                    style={{
-                      height: "unset",
-                      margin: "0 auto",
-                      textAlign: "left",
-                      padding: 0,
-                    }}
-                  >
-                    <Card size="small">
-                      <List.Item.Meta
-                        avatar={
-                          <Avatar>
-                            <Icons.PlusOutlined />
-                          </Avatar>
-                        }
-                        title="Set up your own DAO"
-                      />
-                    </Card>
-                  </CreateOrganizationButton>
-                </Space>
-              </Col>
-            )}
-          </Row>
-        ),
-      }}
-    />
+    <TaskBoard tasks={tasks} empty={empty} />
   ) : (
     <SkeletonTaskBoard />
   );
