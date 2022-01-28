91f0ca92904cfebfc7816bce353091902bc415e0
David Fant <david@fant.io>
2022-01-28 11:25:24 +0100

    improved error message when trying to auth with account that is already connected

--- packages/app/pages/auth/3pid/[threepidId].tsx
+++ packages/app/pages/auth/3pid/[threepidId].tsx
@@ -4,6 +4,8 @@ import { Button, Layout, Modal, Space, Typography } from "antd";
 import { useRouter } from "next/router";
 import { useAuthWithThreepid } from "@dewo/app/containers/auth/hooks";
 import { useAcceptInvite } from "@dewo/app/containers/invite/hooks";
+import { ApolloError } from "@apollo/client";
+import Link from "next/link";
 
 const Auth: NextPage = () => {
   const router = useRouter();
@@ -14,20 +16,25 @@ const Auth: NextPage = () => {
     [stateString]
   );
 
-  const [loading, setLoading] = useState(false);
   const authWithThreepid = useAuthWithThreepid();
   const acceptInvite = useAcceptInvite();
+  const [accountAlreadyConnected, setAccountAlreadyConnected] = useState(false);
 
   const auth = useCallback(async () => {
     try {
-      setLoading(true);
       await authWithThreepid(threepidId);
       if (!!state.inviteId) {
         await acceptInvite(state.inviteId).catch();
       }
       await router.push(state.redirect ?? "/");
-    } finally {
-      setLoading(false);
+    } catch (error) {
+      if (error instanceof ApolloError) {
+        if (error.message === "Account already connected") {
+          setAccountAlreadyConnected(true);
+        }
+
+        throw error;
+      }
     }
   }, [authWithThreepid, threepidId, router, state, acceptInvite]);
   useEffect(() => {
@@ -38,14 +45,38 @@ const Auth: NextPage = () => {
   return (
     <Layout>
       <Layout.Content>
-        <Modal visible footer={null}>
-          <Space direction="vertical" style={{ width: "100%" }}>
+        <Modal visible footer={null} closable={false}>
+          <Space
+            direction="vertical"
+            style={{ width: "100%", alignItems: "center" }}
+          >
             <Typography.Title level={2} style={{ textAlign: "center" }}>
               Connect account
             </Typography.Title>
-            <Button size="large" type="primary" block loading={loading}>
-              Signing in...
-            </Button>
+            {accountAlreadyConnected ? (
+              <>
+                <Typography.Paragraph style={{ textAlign: "center" }}>
+                  This account is already connected to another Dework user.{" "}
+                  <a
+                    href="https://discord.com/channels/918603668935311391/920039372882051122"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Write to us on Discord
+                  </a>{" "}
+                  and we will help you connect to both accounts.
+                </Typography.Paragraph>
+                <Link href="/">
+                  <a>
+                    <Button size="large">Go back</Button>
+                  </a>
+                </Link>
+              </>
+            ) : (
+              <Button size="large" type="primary" block loading>
+                Signing in...
+              </Button>
+            )}
           </Space>
         </Modal>
       </Layout.Content>
--- packages/app/src/contexts/AuthContext.tsx
+++ packages/app/src/contexts/AuthContext.tsx
@@ -18,7 +18,7 @@ export function useCurrentUser(skip: boolean = false): UserDetails | undefined {
 interface AuthContextValue {
   user: UserDetails | undefined;
   authenticated: boolean;
-  logout(): void;
+  logout(path?: string): void;
   onAuthenticated(authToken: string): void;
 }
 
@@ -36,10 +36,10 @@ export const AuthProvider: FC<{ initialAuthenticated: boolean }> = ({
   const [authenticated, setAuthenticated] = useState(initialAuthenticated);
   const user = useCurrentUser(!authenticated);
 
-  const logout = useCallback(() => {
+  const logout = useCallback((path = "/") => {
     clearAuthToken(undefined);
     setAuthenticated(false);
-    window.location.href = "/";
+    window.location.href = path;
   }, []);
 
   const onAuthenticated = useCallback((authToken: string) => {
