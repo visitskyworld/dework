71dde18ab9d0b7d7156433206fc59a69f6c895ab
rafaewaqarqazi <rafaewaqar@gmail.com>
2022-02-15 17:45:26 +0500

    Make twitter button blue, use task permalink

--- packages/app/src/containers/task/form/TaskTwitterShareButton.tsx
+++ packages/app/src/containers/task/form/TaskTwitterShareButton.tsx
@@ -3,32 +3,27 @@ import { TaskDetails } from "@dewo/app/graphql/types";
 import { FormSection } from "@dewo/app/components/FormSection";
 import { Button } from "antd";
 import { TwitterOutlined } from "@ant-design/icons";
-import { useRouter } from "next/router";
-import { Constants } from "@dewo/app/util/constants";
 interface Props {
   task: TaskDetails;
 }
 
-export const TaskTwitterShareButton: FC<Props> = ({ task }) => {
-  const router = useRouter();
-  const url = encodeURIComponent(`${Constants.APP_URL}${router.asPath}`);
-  return (
-    <FormSection label="Twitter Share" className="mb-3">
-      <Button
-        type="primary"
-        target="_blank"
-        size="small"
-        icon={<TwitterOutlined />}
-        href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-          task.name
-        )}:&url=${url}${
-          task.tags?.length
-            ? `&hashtags=${task.tags.map((tag) => tag.label).toString()}`
-            : ""
-        } `}
-      >
-        Share task on twitter
-      </Button>
-    </FormSection>
-  );
-};
+export const TaskTwitterShareButton: FC<Props> = ({ task }) => (
+  <FormSection label="Twitter Share" className="mb-3">
+    <Button
+      type="primary"
+      target="_blank"
+      className="dewo-twitter-btn"
+      size="small"
+      icon={<TwitterOutlined />}
+      href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+        task.name
+      )}:&url=${encodeURIComponent(task.permalink)}${
+        task.tags?.length
+          ? `&hashtags=${task.tags.map((tag) => tag.label).toString()}`
+          : ""
+      } `}
+    >
+      Share task on twitter
+    </Button>
+  </FormSection>
+);
