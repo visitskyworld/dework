5f96b3bb906661d0e2e6c529bd8618b6a46ea217
David Fant <david@fant.io>
2022-02-22 14:41:33 +0100

    updated twitter button copy

--- packages/app/src/containers/task/form/TaskTwitterShareButton.tsx
+++ packages/app/src/containers/task/form/TaskTwitterShareButton.tsx
@@ -3,27 +3,34 @@ import { TaskDetails } from "@dewo/app/graphql/types";
 import { FormSection } from "@dewo/app/components/FormSection";
 import { Button } from "antd";
 import { TwitterOutlined } from "@ant-design/icons";
+import slugify from "slugify";
+import * as qs from "query-string";
+
 interface Props {
   task: TaskDetails;
 }
 
-export const TaskTwitterShareButton: FC<Props> = ({ task }) => (
-  <FormSection label="Twitter Share" className="mb-3">
-    <Button
-      type="primary"
-      target="_blank"
-      className="dewo-twitter-btn"
-      size="small"
-      icon={<TwitterOutlined />}
-      href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-        task.name
-      )}:&url=${encodeURIComponent(task.permalink)}${
-        task.tags?.length
-          ? `&hashtags=${task.tags.map((tag) => tag.label).toString()}`
-          : ""
-      } `}
-    >
-      Share task on twitter
-    </Button>
-  </FormSection>
-);
+export const TaskTwitterShareButton: FC<Props> = ({ task }) => {
+  const text = [`🚨 Bounty Call 🚨`, task.name, "Check out the details👇"].join(
+    "\n\n"
+  );
+  const hashtags = task.tags.map((t) => slugify(t.label)).join();
+  return (
+    <FormSection label="Twitter" className="mb-3">
+      <Button
+        type="primary"
+        target="_blank"
+        className="dewo-twitter-btn"
+        size="small"
+        icon={<TwitterOutlined />}
+        href={`https://twitter.com/intent/tweet?${qs.stringify({
+          text,
+          hashtags,
+          url: task.permalink,
+        })}`}
+      >
+        Share on Twitter
+      </Button>
+    </FormSection>
+  );
+};
