2980993f3d3ef51afc4e25718c1706f937ce4609
David Fant <david@fant.io>
2022-05-16 11:14:59 +0200

    task view toolbar search

--- packages/app/src/components/DebouncedInput.tsx
+++ packages/app/src/components/DebouncedInput.tsx
@@ -1,31 +1,34 @@
 import React, {
   ChangeEventHandler,
-  FC,
+  forwardRef,
   useCallback,
   useMemo,
   useState,
 } from "react";
-import { Input, InputProps } from "antd";
+import { Input, InputProps, InputRef } from "antd";
 import _ from "lodash";
 
-export const DebouncedInput: FC<InputProps & { debounce?: number }> = ({
-  debounce = 300,
-  onChange,
-  value,
-  ...props
-}) => {
-  const debouncedUpdate = useMemo(
-    () => (onChange ? _.debounce(onChange, debounce) : () => {}),
-    [debounce, onChange]
-  );
-  const [currentVal, setValue] = useState(value);
-  const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback(
-    (e) => {
-      setValue(e.target.value);
-      debouncedUpdate(e);
-    },
-    [debouncedUpdate]
-  );
+interface Props extends InputProps {
+  debounce?: number;
+}
 
-  return <Input {...props} value={currentVal} onChange={handleChange} />;
-};
+export const DebouncedInput = forwardRef<InputRef, Props>(
+  ({ debounce = 300, onChange, value, ...props }, ref) => {
+    const debouncedUpdate = useMemo(
+      () => (onChange ? _.debounce(onChange, debounce) : () => {}),
+      [debounce, onChange]
+    );
+    const [currentVal, setValue] = useState(value);
+    const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback(
+      (e) => {
+        setValue(e.target.value);
+        debouncedUpdate(e);
+      },
+      [debouncedUpdate]
+    );
+
+    return (
+      <Input ref={ref} {...props} value={currentVal} onChange={handleChange} />
+    );
+  }
+);
--- packages/app/src/containers/task/views/TaskViewToolbar.tsx
+++ packages/app/src/containers/task/views/TaskViewToolbar.tsx
@@ -1,14 +1,18 @@
-import React, { ChangeEventHandler, FC, useCallback } from "react";
+import React, {
+  ChangeEventHandler,
+  FC,
+  useCallback,
+  useEffect,
+  useRef,
+} from "react";
 import { FilterIcon } from "@dewo/app/components/icons/Filter";
 import { SortIcon } from "@dewo/app/components/icons/Sort";
-import { Button, Row } from "antd";
+import { Button, InputRef, Row } from "antd";
 import styles from "./TaskViewTabs.module.less";
 import { DebouncedInput } from "@dewo/app/components/DebouncedInput";
 import { useTaskViewContext } from "./TaskViewContext";
 
-interface Props {}
-
-export const TaskViewToolbar: FC<Props> = ({}) => {
+export const TaskViewToolbar: FC = () => {
   const { searchQuery, onSearchQueryChange } = useTaskViewContext();
   const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
     (e) => onSearchQueryChange(e.target.value),
@@ -20,6 +24,22 @@ export const TaskViewToolbar: FC<Props> = ({}) => {
     ) as HTMLButtonElement | undefined;
     maybeButton?.click();
   }, []);
+
+  const input = useRef<InputRef>(null);
+
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      if (e.key === "f3" || ((e.ctrlKey || e.metaKey) && e.key === "f")) {
+        if (!!input.current) {
+          e.preventDefault();
+          input.current.focus();
+        }
+      }
+    };
+    window.addEventListener("keydown", listener);
+    return () => window.removeEventListener("keydown", listener);
+  }, []);
+
   return (
     <Row align="middle" className={styles.toolbar}>
       <Button
@@ -41,6 +61,7 @@ export const TaskViewToolbar: FC<Props> = ({}) => {
         Filter
       </Button>
       <DebouncedInput
+        ref={input}
         value={searchQuery}
         placeholder="Search tasks..."
         style={{ flex: 1, maxWidth: 200 }}
