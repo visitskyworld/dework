136882d62af6f2fe715f37e6e93961988e2fd4be
davidfant <david@fant.io>
2022-05-26 18:53:09 +0200

    backfill github integration for projects that used to have it enabled (#725)

---
+++ packages/api/src/migrations/1653582887510-CreateFallbackGithubIssueSyncIntegrations.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateFallbackGithubIssueSyncIntegrations1653582887510
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const integrations: {
+      organizationIntegrationId: string;
+      creatorId: string;
+      projectId: string;
+      organization: string;
+      repo: string;
+    }[] = await queryRunner.query(`
+      SELECT
+        project_integration."organizationIntegrationId",
+        project_integration."projectId",
+        project_integration."creatorId",
+        project_integration.config->'organization' AS organization,
+        project_integration.config->'repo' AS repo
+      FROM project_integration
+      WHERE "type" = 'GITHUB'
+        AND "createdAt" < '2022-05-12'
+        AND "deletedAt" IS NULL
+        AND (config->'features')::jsonb ? 'SHOW_BRANCHES'
+    `);
+
+    await queryRunner.query(`
+      INSERT INTO project_integration ("organizationIntegrationId", "projectId", "creatorId", "config", "type")
+      VALUES ${integrations
+        .map(
+          (i) =>
+            `('${i.organizationIntegrationId}', '${i.projectId}', '${
+              i.creatorId
+            }', '${JSON.stringify({
+              organization: i.organization,
+              repo: i.repo,
+              features: ["CREATE_TASKS_FROM_ISSUES"],
+            })}', 'GITHUB')`
+        )
+        .join(",\n")}
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
